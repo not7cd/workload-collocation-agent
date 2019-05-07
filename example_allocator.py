afd2e6261ebbb059a02327e4abacad807099f16a
Pawel Palucki <pawel.palucki@intel.com>
2019-05-07 23:30:24 +0200

    logging and fixes

--- example_allocator.py
+++ example_allocator.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
 from owca.allocators import Allocator, RDTAllocation, Metric
-
+import logging
+log = logging.getLogger(__name__)
 
 THROTTLE = {
     'cpu_quota': 0.,
     'rdt': RDTAllocation(
         name='best-effort',
-        l3='L3:0=00001;1=00001',
-        mb='MB:0=5;1=5'
+        l3='L3:0=002;1=002',
+        mb='MB:0=10;1=10'
     )
 }
 
@@ -26,13 +27,18 @@ class ExampleAllocator(Allocator):
 
     preasure_threshold: float = 1.
 
+    def __post_init__(self):
+        log.info('Example allocator with preasure_threshold: %r', self.preasure_threshold)
+
     def allocate(self, platform, measurements, resources, labels, allocations):
 
         # Calculate preasure ...
         total_preasure = 0.
         for task in measurements:
-            if labels[task].get('task_kind') != 'best-effort':
-                if measurements[task].get('ipc', 1.0) < 1.0:
+            if labels[task].get('task_kind') == 'latency-critical':
+                ipc = measurements[task].get('ipc', 1.0)
+                log.debug('found latency critical task with ipc:',  ipc)
+                if ipc < 1.:
                     total_preasure += 1/measurements[task]['ipc']
         metrics = [
             Metric(name='total_preasure', value=total_preasure)
